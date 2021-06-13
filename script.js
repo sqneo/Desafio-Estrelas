b837b6a6a441b2918ecb5f6ebec759b40fadde83
sqneo <deehferreira1999@gmail.com>
2021-06-13 14:03:43 -0300

    inicio

---
+++ script.js
@@ -0,0 +1,23 @@
+const data =[
+    {
+        image: document.querySelector('#img0'),
+        name: document.querySelector('#name0'),
+        
+    },
+    {
+        image: document.querySelector('img1'),
+        name: document.querySelector('#name1'),
+        
+    },
+    {
+        image: document.querySelector('img2'),
+        name: document.querySelector('#name2'),
+        
+    },
+    {
+        image: document.querySelector('img3'),
+        name: document.querySelector('#name3'),
+        
+    }
+]
+
